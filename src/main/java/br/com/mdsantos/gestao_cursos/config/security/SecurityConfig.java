050885a2da57286ea8a7b9bf9c9d564393fcc309
Michel Santos <michelsantosk2@gmail.com>
2025-08-13 08:48:15 -0300

    feat: start jws login

--- src/main/java/br/com/mdsantos/gestao_cursos/config/security/SecurityConfig.java
+++ src/main/java/br/com/mdsantos/gestao_cursos/config/security/SecurityConfig.java
@@ -7,13 +7,29 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private static final String[] AUTH_WHITELIST = {
+            "/auth/**",
+            "/users/"
+    };
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                auth -> {
+                    auth.requestMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated();
+                }
+        ).addFilterBefore(this.securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
